0776b12ce448d10b08ee162334e72ed8f6de40d2
Ian <ian@somewhere.com>
2024-06-29 16:04:30 +0200

    [!] Initial commit.

---
+++ src/me_StoredCall.h
@@ -0,0 +1,40 @@
+// Store handler and call it later
+
+/*
+  Author: Martin Eden
+  Version: 1
+  Last mod.: 2024-06-29
+*/
+
+#pragma once
+
+#include <me_BaseTypes.h>
+
+namespace me_StoredCall
+{
+  using
+    me_BaseTypes::TMethod,
+    me_BaseTypes::TUint_2;
+
+  /*
+    Delayed method call
+
+    Store handler now, call it later.
+  */
+  struct TStoredCall
+  {
+    TMethod Handler;
+    TUint_2 Upvalues;
+
+    // Zeroing init
+    TStoredCall(): Handler(0), Upvalues(0) {};
+    // Store handler and upvalues
+    void Set(TMethod Handler, TUint_2 Upvalues);
+    // Call with external data
+    void Run(TUint_2 Data = 0);
+  };
+}
+
+/*
+  2024-06-29
+*/
