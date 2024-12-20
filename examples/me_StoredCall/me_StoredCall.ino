e71b9ee17003518399943b99898a42e734740b22
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-12-20 16:10:13 +0200

    [~] [Example] Sync with [me_Uart].

--- examples/me_StoredCall/me_StoredCall.ino
+++ examples/me_StoredCall/me_StoredCall.ino
@@ -2,18 +2,18 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-10-23
+  Last mod.: 2024-12-20
 */
 
 #include <me_StoredCall.h>
 
 #include <me_BaseTypes.h>
-#include <me_UartSpeeds.h>
+#include <me_Uart.h>
 #include <me_Console.h>
 
 void setup()
 {
-  Serial.begin(me_UartSpeeds::Arduino_Normal_Bps);
+  me_Uart::Init(me_Uart::Speed_115k_Bps);
 
   Console.Print("[me_StoredCall] We are here.");
   Test();
